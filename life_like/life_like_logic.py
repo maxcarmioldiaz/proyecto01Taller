29bdf4e349bf933f8433150086643ff53149c4d2
Maximiliano Carmiol D. <carmioldiazmaximiliano@gmail.com>
2026-05-28 10:35:23 -0600

    Def's de subrutinas bases

--- life_like/life_like_logic.py
+++ life_like/life_like_logic.py
@@ -1,20 +1,24 @@
-def generar_matriz_aleatoria():
+from random import randint
+
+
+def generar_matriz_aleatoria(filas, columnas):
+    """
+    
+    """
+    return [[randint(0 , 1) for c in range(columnas)] for f in range(filas)]
+
+def generar_matriz_vacia(filas, columnas):
+    """
+    
+    """
+    return [[0 for c in range(columnas)] for f in range(filas)]
+
+def transicion_celula(estado, vecinos):
     """
     
     """
 
-def generar_matriz_vacia():
+def transicion(M):
     """
     
     """
-
-def transicion_celula():
-    """
-    
-    """
-
-def transicion():
-    """
-    
-    """
-    
