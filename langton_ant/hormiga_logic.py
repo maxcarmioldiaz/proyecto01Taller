f61549c7e0e747424f4fb088d2dd9947f3a3a200
Maximiliano Carmiol D. <carmioldiazmaximiliano@gmail.com>
2026-05-28 10:10:16 -0600

    Initial Commit

---
+++ langton_ant/hormiga_logic.py
@@ -0,0 +1,20 @@
+def girar_hormiga(direccion):
+    """
+    
+    """
+
+def avanzar_hormiga():
+    """
+    
+    """
+
+def siguiente():
+    """
+    
+    """
+
+def generar_colores(n):
+    """
+    
+    """
+
---
+++ life_like/life_like_logic.py
@@ -0,0 +1,20 @@
+def generar_matriz_aleatoria():
+    """
+    
+    """
+
+def generar_matriz_vacia():
+    """
+    
+    """
+
+def transicion_celula():
+    """
+    
+    """
+
+def transicion():
+    """
+    
+    """
+    
